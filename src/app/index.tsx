0076ef685e8e4e94ec418c35b69f6f721bfa520f
MakoStudyRoom <134215847+MakoStudyRoom@users.noreply.github.com>
2026-01-01 23:10:30 +0900

    Markup memo list screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,68 @@
-import {View,Text}from 'react-native'
+import { View, Text , StyleSheet } from 'react-native'
 
-const Index = () : React.JSX.Element =>{
-    return(
-        <View>
-            <Text>Hello World2</Text>
+const Index = (): React.JSX.Element => {
+    return (
+        <View style={styles.container}>
+            <View>
+                {/* ヘッダー */}
+                <Text>Memo App</Text>
+                <Text>ログアウト</Text>
+            </View>
+
+            {/* メモ1 */}
+            <View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026年1月1日</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+            </View>
+            {/* メモ2 */}
+            <View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026年1月1日</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+            </View>
+            {/* メモ3 */}
+            <View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026年1月1日</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+            </View>
+            <View>
+                {/* + */}
+                <Text>+</Text>
+            </View>
+
+
+
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems:'center'
+    }
+})
 
 export default Index
